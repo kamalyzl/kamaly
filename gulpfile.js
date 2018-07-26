a978b0f69ea377e76486e5f0526f03c65068e127
kamalyzl <kamaly.kazal@gmail.com>
2018-07-26 03:23:26 -0500

    cambiando html plano con pug

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const pug = require('gulp-pug')
+const babel = require('gulp-babel');
+const autoprefixer = require('gulp-autoprefixer')
+
+
+gulp.task('pug', () => {
+  gulp.src('./dev/view/*.pug')
+    .pipe(pug({
+      pretty: true
+    })) // pipe coge los datos y los pasa por plugins
+    .pipe(gulp.dest('./public')) // dest es la carpeta de destino
+});
+
+gulp.task('css', () => {
+  gulp.src('./dev/sass/*.scss')
+    .pipe(sass({
+      outputStyle: 'compact',
+      sourceComments: true,
+    }))
+    .pipe(autoprefixer({
+      versions:['last 2 browsers']
+    }))
+    .pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('js', () => {
+  gulp.src('./dev/js/*.js')
+    .pipe(babel({
+      "presets": ["env"]
+    }))
+    // .pipe(init())
+    // .pipe(concat('app.min.js'))
+    // .pipe(write())
+    .pipe(gulp.dest('public/js'))
+});
+
+
+gulp.task('default', () => {
+  gulp.watch('./dev/sass/**/*.scss', ['css']);
+  gulp.watch('./dev/view/**/*.pug', ['pug']);
+  gulp.watch('./dev/js/**/*.js', ['js']);
+});
+
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var prueba = function prueba() {
+  return console.log('renuevo algo');
+};
