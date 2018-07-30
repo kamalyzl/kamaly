85021695da55b9ca0750ae369e50407d8fd8cce7
kamalyzl <kamaly.kazal@gmail.com>
2018-07-30 18:36:08 -0500

    agregando funcionalidad inicial para el nuevo diseño

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,29 @@
-const prueba = () => {
-  return console.log('renuevo algo')
-}
+const home = document.getElementById('home')
+const about = document.getElementById('about')
+const skill = document.getElementById('skill')
+const proyect = document.getElementById('proyect')
+const contact = document.getElementById('contact')
+const container = document.getElementById('container_desktop')
+
+
+
+home.addEventListener('click', () => {
+
+  console.log('home')
+})
+
+about.addEventListener('click', () => {
+  container.innerHTML="p hola"
+})
+
+skill.addEventListener('click', () => {
+  console.log('skill')
+})
+
+proyect.addEventListener('click', () => {
+  console.log('proyect')
+})
+
+contact.addEventListener('click', () => {
+  console.log('contact')
+})
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
-var prueba = function prueba() {
-  return console.log('renuevo algo');
-};
+var home = document.getElementById('home');
+var about = document.getElementById('about');
+var skill = document.getElementById('skill');
+var proyect = document.getElementById('proyect');
+var contact = document.getElementById('contact');
+var container = document.getElementById('container_desktop');
+
+home.addEventListener('click', function () {
+
+  console.log('home');
+});
+
+about.addEventListener('click', function () {
+  container.innerHTML = "p hola";
+});
+
+skill.addEventListener('click', function () {
+  console.log('skill');
+});
+
+proyect.addEventListener('click', function () {
+  console.log('proyect');
+});
+
+contact.addEventListener('click', function () {
+  console.log('contact');
+});
